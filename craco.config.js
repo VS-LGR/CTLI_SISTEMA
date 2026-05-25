c456a84f4817f7e5fec2d3d82735b5609ee12188
Lucas Gabriel Rodrigues <lucas@lgr-design.art>
2026-05-25 14:35:02 -0300

    Fix erro de build

--- craco.config.js
+++ craco.config.js
@@ -37,6 +37,7 @@ const webpackConfig = {
       webpackConfig.resolve.fallback = {
         ...webpackConfig.resolve.fallback,
         buffer: require.resolve("buffer/"),
+        stream: require.resolve("stream-browserify"),
       };
       webpackConfig.plugins = webpackConfig.plugins || [];
       const webpack = require("webpack");
